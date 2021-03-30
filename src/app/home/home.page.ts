0beb3ef0ad9850d5af74a6e4a313828d0629456e
ngodup <ngodupbz@gmail.com>
2021-03-30 18:12:50 +0530

    add new chart

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,177 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  @ViewChild('barCanvas') barCanvas: ElementRef;
+  @ViewChild('doughnutCanvas') doughnutCanvas: ElementRef;
+  @ViewChild('lineCanvas') lineCanvas: ElementRef;
+  @ViewChild('doubleLineCanvas') doubleLineCanvas: ElementRef;
 
-  constructor() {}
+  barChart: any;
+  doughnutChart: any;
+  lineChart: any;
+  doubleLineChart: any;
+
+  dataArray: any = [];
+
+  constructor() { }
+
+  ngAfterViewInit() {
+    this.doubleLineChartMethod();
+    this.barChartMethod();
+    this.doughnutChartMethod();
+    this.lineChartMethod();
+
+  }
+
+  doubleLineChartMethod() {
+
+    this.doubleLineChart = new Chart(this.doubleLineCanvas.nativeElement, {
+      type: 'line',
+      data: {
+        labels: ["Facebook", "Instagram", "Mixed Technologies"],
+        datasets: [
+          {
+            label: "Facebook",
+            data: [150, 250, 145, 100],
+            backgroundColor: "rgba(40,125,200,.5)",
+            borderColor: "rgb(40,100,200)",
+            fill: true,
+            lineTension: 0,
+            radius: 5
+          },
+          {
+            label: "Instagram",
+            data: [30, 90, 151, 220],
+            backgroundColor: "rgba(240,78,71,.5)",
+            borderColor: "rgb(240,78,71)",
+            fill: true,
+            lineTension: 0.2,
+            radius: 5
+          }
+        ]
+      },
+
+      options: {
+        responsive: true,
+        title: {
+          display: true,
+          position: "top",
+          text: "Facebook to Instagram - Social Networking",
+          fontSize: 12,
+          fontColor: "#666"
+        },
+        legend: {
+          display: true,
+          position: "bottom",
+          labels: {
+            fontColor: "#999",
+            fontSize: 14
+          }
+        }
+      }
+    })
+  }
+
+  lineChartMethod() {
+    this.lineChart = new Chart(this.lineCanvas.nativeElement, {
+      type: 'line',
+      data: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'November', 'December'],
+        datasets: [
+          {
+            label: 'Sell per week',
+            fill: false,
+            lineTension: 0.1,
+            backgroundColor: 'rgba(75,192,192,0.4)',
+            borderColor: 'rgba(75,192,192,1)',
+            borderCapStyle: 'butt',
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: 'miter',
+            pointBorderColor: 'rgba(75,192,192,1)',
+            pointBackgroundColor: '#fff',
+            pointBorderWidth: 1,
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+            pointHoverBorderColor: 'rgba(220,220,220,1)',
+            pointHoverBorderWidth: 2,
+            pointRadius: 1,
+            pointHitRadius: 10,
+            data: [65, 59, 80, 81, 56, 55, 40, 10, 5, 50, 10, 15],
+            spanGaps: false,
+          }
+        ]
+      }
+    });
+  }
+
+  barChartMethod() {
+    this.barChart = new Chart(this.barCanvas.nativeElement, {
+      type: 'bar',
+      data: {
+        labels: ['BJP', 'INC', 'AAP', 'CPI', 'CPI-M', 'NCP'],
+        datasets: [{
+          label: '# of Votes',
+          data: [200, 50, 30, 15, 20, 34],
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)'
+          ],
+          borderColor: [
+            'rgba(255,99,132,1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
+          ],
+          borderWidth: 1
+        }]
+      },
+      options: {
+        responsive: true,
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true
+            }
+          }]
+        }
+      }
+    });
+  }
+
+  doughnutChartMethod() {
+    this.doughnutChart = new Chart(this.doughnutCanvas.nativeElement, {
+      type: 'doughnut',
+      data: {
+        labels: ['BJP', 'Congress', 'AAP', 'CPM', 'SP'],
+        datasets: [{
+          label: '# of Votes',
+          data: [50, 29, 15, 10, 7],
+          backgroundColor: [
+            'rgba(255, 159, 64, 0.2)',
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)'
+          ],
+          hoverBackgroundColor: [
+            '#FFCE56',
+            '#FF6384',
+            '#36A2EB',
+            '#FFCE56',
+            '#FF6384'
+          ]
+        }]
+      }
+    });
+  }
+
+
 
 }
